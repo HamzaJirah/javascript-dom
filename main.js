1152cb413411574e6d803f4a6c8aecd876790df6
HamzaJirah <hamzajirah@gmail.com>
2017-12-30 11:20:59 +0300

    created variables, an oclick event and a function

---
+++ main.js
@@ -0,0 +1,10 @@
+var content = document.getElementById("content");
+var button = documnet.getElementById("show-more");
+
+button.onclick = function(){
+  if(content.className = "open"){
+    //shrink box
+  }else{
+    //expand box
+  }
+}
