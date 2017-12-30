0aea7705698c15d33307056d4a9b02b42f29c9bc
HamzaJirah <hamzajirah@gmail.com>
2017-12-30 11:51:14 +0300

    debugged the button variable/if statement

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var content = document.getElementById("content");
-var button = documnet.getElementById("show-more");
+var button = document.getElementById("show-more");
 
 button.onclick = function(){
-  if(content.className = "open"){
-    //shrink box
+  if(content.className == "open"){
+    //shrink size of the box
     content.className = "";
-    button.innerHTML = "Show More";
+    button.innerHTML = "Show More ";
   }else{
-    //expand box
+    //expand size of the box
     content.className = "open";
     button.innerHTML = "Show Less";
   }
