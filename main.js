c191e2d3f614cdbb96aaacc16217c9379a62daf2
HamzaJirah <hamzajirah@gmail.com>
2017-12-30 15:09:24 +0300

    added the window.onload event listener

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
 
-button.onclick = function(){
-  if(content.className == "open"){
-    //shrink size of the box
-    content.className = "";
-    button.innerHTML = "Show More ";
-  }else{
-    //expand size of the box
-    content.className = "open";
-    button.innerHTML = "Show Less";
-  }
+function setUpEvents (){
+  var content = document.getElementById("content");
+  var button = document.getElementById("show-more");
+  
+  button.onclick = function(){
+    if(content.className == "open"){
+      //shrink size of the box
+      content.className = "";
+      button.innerHTML = "Show More ";
+    }else{
+      //expand size of the box
+      content.className = "open";
+      button.innerHTML = "Show Less";
+    }
+  };
 }
+window.onload = function(){
+  setUpEvents();
+};
