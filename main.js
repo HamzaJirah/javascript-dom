d143a4a09b2e1bd28bf3b752f4b92b1d50d8224d
HamzaJirah <hamzajirah@gmail.com>
2017-12-30 11:27:48 +0300

    added js conditionals

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ var button = documnet.getElementById("show-more");
 button.onclick = function(){
   if(content.className = "open"){
     //shrink box
+    content.className = "";
+    button.innerHTML = "Show More";
   }else{
     //expand box
+    content.className = "open";
+    button.innerHTML = "Show Less";
   }
 }
